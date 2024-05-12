4ad59d575de1281e16e73db9a510631d6fe8b527
Ainazik Momunalieva <58284183+ainaziko@users.noreply.github.com>
2024-05-12 19:56:16 +0600

    +functionality to create tasks

---
+++ index.js
@@ -0,0 +1,42 @@
+var tasksList = document.querySelector('.tasks');
+
+
+document.getElementById('todoForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const selectedRadioButton = document.querySelector('input[name="category-option"]:checked');
+
+    if (selectedRadioButton) {
+        console.log("Category: ", selectedRadioButton.className);
+    } else {
+        console.log("Please select a category");
+        return;
+    }
+
+    const task = document.querySelector('input[name="task"]').value;
+    console.log("Task: ", task);
+    
+
+    const todoInfoDiv = document.createElement('div');
+        todoInfoDiv.classList.add('todo-info');
+
+        // Set the content of the div
+        todoInfoDiv.innerHTML = `
+        
+        <div class="task">
+            <div class="task-description">
+                <input type="radio" class="${selectedRadioButton.className}">
+                <p class="task__text">${task}</p>
+            </div>
+            
+            <div class="btns">
+                <button class="edit-task">Edit</button>
+                <button class="delete-task">Delete</button>
+            </div>
+        </div>
+        
+        `;
+
+        tasksList.appendChild(todoInfoDiv);
+});
+
